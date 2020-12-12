0fcefe52010964d07043b1c387e1ff45436c64d2
Minjae Cho <ussr1285@gmail.com>
2020-12-12 23:20:12 +0900

    웹툰 정보 긁어오기 및 댓글 긁어오기 개발 완료

--- Term2_202021511_조민재.R
+++ Term2_202021511_조민재.R
@@ -1,40 +1,135 @@
 library(httr)
-library(xml2)
 library(rvest)
-install.packages("rvest")
+library(RSelenium)
+library(seleniumPipes)
+library(tidyverse)
 
-title_id <- "703852" # 웹툰 개별 id, 벡터로 여러가지 담아야 함.
+clean_text <- function(arg_text){
+  arg_text <- gsub("\n", "", arg_text)
+  arg_text <- gsub("\t", "", arg_text)
+  arg_text <- trimws(arg_text)
+  return(arg_text)
+}
 
-# title_id에 따른 반복문
-n_webtoon_url <- "https://comic.naver.com/webtoon/list.nhn"
-n_webtoon_source <- GET(n_webtoon_url,
-                        query=list(titleId=title_id)) %>%
-  read_html(content(res, as="text"))
+remD <- remoteDriver(port = 4445L, browserName = "chrome") # 포트번호 입력, 사용할 브라우저
+remD$open() # 서버에 연결
 
-last_episode <- n_webtoon_source %>%
+# 댓글 담을 df
+comment_df <- tibble(
+  id <- c(),
+  episode <- c(),
+  amount_comments <- c(),
+  episode_comments <- c(),
+  episode_user <- c(),
+  comments_good <- c()
+)
+colnames(comment_df) <- c("id", "episode", "amount_comments", "episode_comments", "episode_user", "comments_good")
+
+# 평점 및 좋아요 참여자 담을 df
+evaluate_df <- tibble(
+  id <- c(),
+  episode <- c(),
+  starpoint <- c(),
+  participant <- c(),
+  heart <- c()
+)
+
+# 웹툰 정보 담을 df
+info_df <- tibble(
+  id <- c(),
+  title <- c(),
+  cartoonist <- c(),
+  genre <- c(),
+  last_episode <- c()
+)
+
+# 변수
+main_url <- "https://comic.naver.com/webtoon/weekday.nhn"
+list_url <- "https://comic.naver.com/webtoon/list.nhn"
+webtoon_url <- "https://comic.naver.com/webtoon/detail.nhn"
+comment_url <- "https://comic.naver.com/comment/comment.nhn"
+
+
+###### 크롤링 시작 ######
+
+# 웹툰 개별 id
+title_id <- "703852" 
+
+# 웹툰 리스트 화면
+list_source <- GET(list_url,
+                   query=list(titleId=title_id)) %>%
+  read_html()
+
+# 작가 파악
+cartoonist <- list_source %>%
+  html_nodes(".wrt_nm") %>%
+  html_text()
+cartoonist <- clean_text(cartoonist) # 전처리 필요
+
+# 제목 파악
+webtoon_title <- list_source %>%
+  html_nodes("h2") %>%
+  html_text()
+webtoon_title <- clean_text(webtoon_title[[2]])
+webtoon_title <- gsub(cartoonist, "", webtoon_title)
+
+# 장르 파악
+genre <- list_source %>%
+  html_nodes(".genre") %>%
+  html_text()
+
+# 마지막 화 파악
+last_episode <- list_source %>%
   html_nodes(".v2+ tr a") %>%
   html_text()
 last_episode <- last_episode[[2]]
 
-n_comment_url <- paste0("https://comic.naver.com/comment/comment.nhn") # ?titleId=", title_id 
-
-#for(i in 1:last_episode){
-  n_episode_comment_source <- GET(n_comment_url, 
-                                query=list(title_id=title_id, no=1)) %>% # i
+for(i in 1:2){ # last_episode
+  ## 평가 크롤링
+  
+  # 웹툰 화면
+  remD$navigate(paste0(webtoon_url, "?titleId=", title_id,"&no=",i))
+  webtoon_source <- remD$getPageSource()[[1]] %>%
     read_html()
   
-  View(n_episode_comment_source)
-  
-  amount_comments <- n_episode_comment_source %>%
-  html_nodes(".u_cbox_title , .u_cbox_count") %>%
-  html_text()
-
-  episode_comments <- n_comment_source %>%
-    html_nodes("._user_id_no_3to9K .u_cbox_contents") %>%
+  # 별점
+  webtoon_star <- webtoon_source %>%
+    html_nodes("#bottomPointTotalNumber strong") %>%
     html_text()
-#}
+  
+  # 별점 참여자
+  webtoon_participate <- webtoon_source %>%
+    html_nodes("#bottomTotalStarPoint .pointTotalPerson em") %>%
+    html_text()
+  
+  # 하트 수
+  webtoon_heart <- webtoon_source %>%
+    html_nodes(".u_cnt") %>%
+    html_text()
+  
+  
+  ## 댓글 크롤링
+  remD$navigate(paste0(comment_url, "?titleId=", title_id, "&no=",i))
+  episode_comment_source <- remD$getPageSource()[[1]] %>%
+    read_html()
+  
+  temp_amount_comments <- episode_comment_source %>% 
+    html_nodes(".u_cbox_count") %>%
+    html_text()
+  
+  temp_episode_comments <- episode_comment_source %>%
+    html_nodes(".u_cbox_contents") %>%
+    html_text()
+  
+  temp_episode_user <- episode_comment_source %>%
+    html_nodes(".u_cbox_nick") %>%
+    html_text() 
+  
+  temp_comments_good <- episode_comment_source %>%
+    html_nodes(".u_cbox_cnt_recomm") %>%
+    html_text() 
+  
+  comment_df <- add_row(df, id = title_id, episode = i, amount_comments = temp_amount_comments, episode_comments = temp_episode_comments, episode_user = temp_episode_user, comments_good = temp_comments_good)
+  
+}
 
-
-youtube_title <- gsub("\n", "", youtube_title)
-youtube_title <- trimws(youtube_title)
-youtube_title[1:10] 
