e53ebc3836989807d6c09c33ce47b24591e0b3df
Minjae Cho <ussr1285@gmail.com>
2020-12-13 11:30:59 +0900

    testing evaluate_df work
    아직 테스트중

--- temp code.R
+++ temp code.R
@@ -3,26 +3,31 @@
 remD <- remoteDriver(port = 4445L, browserName = "chrome", extraCapabilities = eCaps) # 포트번호 입력, 사용할 브라우저
 remD$open() # 서버에 연결
 
-for(i_id in 1:length(title_ids)){ # 
+title_ids <- info_df$id
+for(i_id in 15:length(title_ids)){ # 
+  # 마지막화
+  print(paste0("id = ", i_id))
   title_id <- title_ids[[i_id]]
+  last_episode <- info_df$last_episode[i_id]
+  
   # 첫화부터 마지막 화까지 for문으로 돌음.
   for(i in 1:last_episode){
     print(paste0("episode : ", i))
     # 웹툰 화면
+    
     remD$navigate(paste0(webtoon_url, "?titleId=", title_id,"&no=",i))
     webtoon_source <- remD$getPageSource()[[1]] %>%
       read_html()
     
     # 별점
     webtoon_star <- webtoon_source %>%
-      html_nodes("#bottomPointTotalNumber strong") %>%
+      html_nodes("#topPointTotalNumber strong") %>%
       html_text()
     
     # 별점 참여자
     webtoon_participate <- webtoon_source %>%
-      html_nodes("#bottomTotalStarPoint .pointTotalPerson em") %>%
+      html_nodes("#topTotalStarPoint .pointTotalPerson em") %>%
       html_text()
-    print(webtoon_participate)
     
     # 하트 수
     webtoon_heart <- webtoon_source %>%
